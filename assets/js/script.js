f50a64b26d58277ad8247fd20400e91a89457172
gabriel <gabriel.gregorio1987@gmail.com>
2023-09-15 04:04:38 -0300

    Creating a main,section,divUl and ul

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+const mainn = document.createElement('main');
+const section = document.createElement('section');
+const divUl = document.createElement('div');
+const ulPokemons = document.createElement('ul');
