4f1ace4c7697e6b97a2cac93b6c61d0df606ab53
gabriel <gabriel.gregorio1987@gmail.com>
2023-09-15 04:09:20 -0300

    Creating an ulStyle function

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,16 @@ const section = document.createElement('section');
 const divUl = document.createElement('div');
 const ulPokemons = document.createElement('ul');
 
+const ulStyle = () => {
+    const ulStyle = ulPokemons.style;
+    ulPokemons.className = 'pokemons';
+    ulStyle.display = 'grid';
+    ulStyle.listStyle = 'none';
+    ulStyle.gridTemplateColumns = '1fr 1fr';
+    ulStyle.padding = '0';
+    ulStyle.margin = '0';
+}
+
 
 section.appendChild(divUl)
 mainn.appendChild(section)
