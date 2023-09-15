4f3b03ae43271cfbd494083b1a14174a9b5b22f9
gabriel <gabriel.gregorio1987@gmail.com>
2023-09-15 04:06:06 -0300

    AppendingChild divUl in section

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,6 @@ const mainn = document.createElement('main');
 const section = document.createElement('section');
 const divUl = document.createElement('div');
 const ulPokemons = document.createElement('ul');
+
+
+section.appendChild(divUl)
